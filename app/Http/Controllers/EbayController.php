40f85cd4e0c6cd3ab3af5f11d09ce780e7c52105
amit834 <amitdeveloper99@gmail.com>
2024-04-06 16:22:24 +0530

    new

--- app/Http/Controllers/EbayController.php
+++ app/Http/Controllers/EbayController.php
@@ -10,29 +10,31 @@ class EbayController extends Controller
     public function auth()
     {
         $clientId = env('EBAY_CLIENT_ID');
-        $redirectUri = urlencode(env('EBAY_REDIRECT_URI'));
+        $redirectUri = env('EBAY_REDIRECT_URI');
         
-        $authUrl = "https://auth.sandbox.ebay.com/oauth2/authorize?client_id={$clientId}&redirect_uri={$redirectUri}&response_type=code&scope=https://api.ebay.com/oauth/api_scope";
+        $authUrl = "https://auth.sandbox.ebay.com/oauth2/authorize?client_id={$clientId}&redirect_uri=" . urlencode($redirectUri) . "&response_type=code&scope=https://api.ebay.com/oauth/api_scope";
         
+        // Redirect the user to eBay authorization page
         return redirect()->away($authUrl);
     }
-
+  
     public function callback(Request $request)
+    
     {
+   
         $clientId = env('EBAY_CLIENT_ID');
         $clientSecret = env('EBAY_CLIENT_SECRET');
         $redirectUri = env('EBAY_REDIRECT_URI');
 
         $code = $request->query('code');
-
-        $response = Http::asForm()->post('https://api.ebay.com/identity/v1/oauth2/token', [
+          $response = Http::asForm()->post('https://api.ebay.com/identity/v1/oauth2/token', [
             'grant_type' => 'authorization_code',
             'code' => $code,
             'redirect_uri' => $redirectUri,
             'client_id' => $clientId,
             'client_secret' => $clientSecret,
         ]);
-
+        print $response->getBody();
         $accessToken = $response->json()['access_token'];
 
         // You can store $accessToken in the session or database for future use.
